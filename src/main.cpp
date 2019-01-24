9e2dddac056179c07247aac279749b815c3e5f88
Marcus Klein <marcus.klein@open-xchange.com>
2019-01-24 11:52:06 +0100

    added deep sleep, use Homie loop and setup handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #define FW_NAME "temp-esp8266"
-#define FW_VERSION "0.9.0"
-
-// GPIO0
-const int PIN_BUTTON = 0;
+#define FW_VERSION "0.9.1"
 
 const int PIN_SDA = 0;
 const int PIN_SCL = 2;
@@ -22,9 +19,28 @@ void onHomieEvent(const HomieEvent & event) {
     case HomieEventType::WIFI_DISCONNECTED:
       WiFi.disconnect();
       break;
+    case HomieEventType::MQTT_READY:
+      Homie.getLogger() << "MQTT connected, preparing for deep sleep..." << endl;
+      Homie.prepareToSleep();
+      break;
+    case HomieEventType::READY_TO_SLEEP:
+      Homie.getLogger() << "Ready to sleep for " << ESP.deepSleepMax() << endl;
+      // microseconds
+      Homie.doDeepSleep(1000000UL);
+      break;
     default:
       break;
   }
+}
+
+void setupHandler() {
+  // advertise units
+  bme280Node.setupHandler();
+}
+
+void loopHandler() {
+  // We want to OTA handler to run only in the loop when WiFi is connected
+  otaLoop();
 }
 
 void setup() {
@@ -34,17 +50,20 @@ void setup() {
   Wire.begin(PIN_SDA, PIN_SCL);
 
   Homie_setFirmware(FW_NAME, FW_VERSION);
-  // Homie.setResetTrigger(PIN_BUTTON, LOW, 5000);
+  // GPIO0 is used for I²C SDA
   Homie.disableResetTrigger();
   // LED_BUILTIN is connected to GPIO1, which is TXD, too. So either use LED or serial console.
   Homie.disableLedFeedback();
 
   Homie.onEvent(onHomieEvent);
   otaSetup(OTA_PORT, OTA_PASS);
+
+  Homie.setSetupFunction(setupHandler);
+  Homie.setLoopFunction(loopHandler);
+
   Homie.setup();
 }
 
 void loop() {
   Homie.loop();
-  otaLoop();
 }
