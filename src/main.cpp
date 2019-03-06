6653bef05dab3536b21e60f6d9e6c4fde7d1f61f
Marcus Klein <himself@kleini.org>
2019-03-06 20:33:14 +0100

    use Timer to postpone deep sleep a bit
    removed OTA
    use Adafruit BME280

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,32 @@
 #define FW_NAME "temp-esp8266"
-#define FW_VERSION "0.9.1"
+#define FW_VERSION "0.9.2"
 
 const int PIN_SDA = 0;
 const int PIN_SCL = 2;
-const int I2C_BME280_ADDRESS = 0x76;
-
-#define OTA_PASS "lala"
-#define OTA_PORT 8266
+const int I2C_BME280_ADDRESS = 0x77;
 
 #include <Homie.h>
-#include "ota.hpp"
 #include "BME280Node.hpp"
-#include <Int64String.h>
+#include "Timer.h"
 
-BME280Node bme280Node("bme280", I2C_BME280_ADDRESS);
+BME280Node bme280Node("bme280", I2C_BME280_ADDRESS, 60);
+Timer t;
+
+void prepareSleep() {
+  Homie.prepareToSleep();
+}
 
 void onHomieEvent(const HomieEvent & event) {
   switch (event.type) {
-    case HomieEventType::WIFI_DISCONNECTED:
-      WiFi.disconnect();
-      break;
     case HomieEventType::MQTT_READY:
-      Homie.getLogger() << "MQTT connected, preparing for deep sleep..." << endl;
-      Homie.prepareToSleep();
+      t.after(500, prepareSleep);
       break;
     case HomieEventType::READY_TO_SLEEP:
-      // TODO synchronize Homie status interval, BME280Node interval and sleep time.
-      // TODO wire from GPIO16 to RST
-      // TODO remove LEDs
-      Homie.getLogger() << "Ready to sleep for " << int64String(ESP.deepSleepMax()) << endl;
-      // microseconds
-      Homie.doDeepSleep(1000000UL);
+      Homie.doDeepSleep(60000000UL);
       break;
     default:
       break;
   }
-}
-
-void setupHandler() {
-  // advertise units
-  bme280Node.setupHandler();
-}
-
-void loopHandler() {
-  // We want to OTA handler to run only in the loop when WiFi is connected
-#if WITH_OTA
-  otaLoop();
-#endif
 }
 
 void setup() {
@@ -62,16 +42,11 @@ void setup() {
   Homie.disableLedFeedback();
 
   Homie.onEvent(onHomieEvent);
-#if WITH_OTA
-  otaSetup(OTA_PORT, OTA_PASS);
-#endif
-
-  Homie.setSetupFunction(setupHandler);
-  Homie.setLoopFunction(loopHandler);
 
   Homie.setup();
 }
 
 void loop() {
   Homie.loop();
+  t.update();
 }
